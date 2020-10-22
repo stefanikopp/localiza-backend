8f819fe54cb6f59d484777ea8609ef754e4b22c8
stefanikopp <stefani.kopp@realizecfi.com.br>
2020-10-22 19:19:05 -0300

    Inicia projeto back end localiza

---
+++ src/App/Application.java
@@ -0,0 +1,15 @@
+package com.company.view;
+
+import com.company.controller.ClienteController;
+import com.company.controller.CompraController;
+import com.company.controller.ProdutoController;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Inicio");
+    }
+}
