79ddf7f2a4a80494dcaa2d6175abc0820e94a196
stefanikopp <stefani.kopp@realizecfi.com.br>
2020-10-23 00:12:51 -0300

    Implementa busca

--- app/controllers/pedido.controller.js
+++ app/controllers/pedido.controller.js
@@ -2,6 +2,9 @@ const Cliente = require("../models/cliente.model.js");
 const Ecommerce = require("../models/ecommerce.model.js");
 const Pedido = require("../models/pedido.model.js");
 const Produto = require("../models/produto.model.js");
+const Sequelize = require('sequelize');
+const { where } = require("sequelize");
+const Op = Sequelize.Op
 
 exports.findAll = async (req, res) => {
   const pedidos = await Pedido.findAll({ include: [
@@ -12,3 +15,38 @@ exports.findAll = async (req, res) => {
 
   res.status(200).send(pedidos);
 };
+
+exports.findByEcommerceName = async (req, res) => {
+  const pedidos = await Pedido.findAll({ 
+    include: [
+    { model: Cliente },
+    { model: Ecommerce, 
+      where: {
+        nome: {
+          [Op.startsWith]: req.params.name
+        }
+      }
+    },
+    { model: Produto },
+  ],
+})
+
+  res.status(200).send(pedidos);
+};
+
+exports.findByStatus = async (req, res) => {
+  const pedidos = await Pedido.findAll({ include: [
+    { model: Cliente },
+    { model: Ecommerce },
+    { model: Produto },
+  ],
+    where: {
+      status: {
+        [Op.like]: req.params.status
+      }
+    }
+  })
+
+  res.status(200).send(pedidos);
+};
+
--- app/routes/pedido.routes.js
+++ app/routes/pedido.routes.js
@@ -2,4 +2,8 @@ module.exports = app => {
   const pedidos = require("../controllers/pedido.controller.js");
 
   app.get("/pedidos", pedidos.findAll);
+
+  app.get("/pedidos/ecommerce/:name", pedidos.findByEcommerceName)
+
+  app.get("/pedidos/:status", pedidos.findByStatus)
 };
