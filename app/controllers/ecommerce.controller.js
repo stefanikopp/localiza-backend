9fa8adc4062aacf83ce05bfb32b042198b32381d
stefanikopp <stefani.kopp@realizecfi.com.br>
2020-10-23 08:57:10 -0300

    Implementa cadastro ecommerce

--- app/controllers/ecommerce.controller.js
+++ app/controllers/ecommerce.controller.js
@@ -1,5 +1,25 @@
 const Ecommerce = require("../models/ecommerce.model.js");
 
+exports.create = async (req, res) => {
+  if (!req.body) {
+    res.status(400).send({ message: "Content can not be empty!" });
+  }
+
+  try {
+    const ecommerceCriado = await Ecommerce.create({
+      nome: req.body.nome,
+      endereco: req.body.endereco,
+      telefone: req.body.telefone
+    })
+
+    res.status(201).send(ecommerceCriado)
+  } catch (error) {
+    res.status(500).send({
+      message: error.message || "Erro ao criar o ecommerce."
+    });
+  }  
+};
+
 exports.findAll = async (req, res) => {
   try {
     const ecommerces = await Ecommerce.findAll()
--- app/routes/ecommerce.routes.js
+++ app/routes/ecommerce.routes.js
@@ -1,6 +1,8 @@
 module.exports = app => {
   const ecommerces = require("../controllers/ecommerce.controller.js");
 
+  app.post("/ecommerces", ecommerces.create);
+
   app.get("/ecommerces", ecommerces.findAll);
 
   app.get("/ecommerces/:id", ecommerces.findOne);
